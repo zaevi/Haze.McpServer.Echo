9ce076d396cb3bd1202a219d274d415823832bbe
Zae <zaevi@live.com>
2025-03-22 21:22:35 +0800

    Update package

--- Program.cs
+++ Program.cs
@@ -1,23 +1,19 @@
-﻿using McpDotNet;
-using McpDotNet.Protocol.Types;
-using McpDotNet.Server;
+﻿using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Hosting;
+using ModelContextProtocol;
 using System.ComponentModel;
 
 var builder = Host.CreateEmptyApplicationBuilder(null);
 builder.Services.AddMcpServer()
     .WithStdioServerTransport()
-    .WithListResourcesHandler((_, _) => Task.FromResult(new ListResourcesResult { Resources = [] }))
-    .WithListPromptsHandler((_, _) => Task.FromResult(new ListPromptsResult { Prompts = [] }))
-    .WithTools();
+    .WithTools(AIFunctionFactory.Create(EchoTool.Echo));
 
 var app = builder.Build();
 await app.RunAsync();
 
-
-[McpToolType]
 public static class EchoTool
 {
-    [McpTool, Description("Echoes the message back to the client.")]
-    public static string Echo(string message) => $"hello {message}";
+    [Description("Echoes the message back to the client.")]
+    public static string Echo([Description("message content")] string message) => $"hello {message}";
+
 }
